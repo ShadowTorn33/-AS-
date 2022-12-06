70e37704c039c21d27c87e9460d3a1369a3ec234
ShadowTorn33 <austincheer94@gmail.com>
2022-12-06 11:30:17 -0800

    Added api to homepage

--- all-sports-app/src/App.js
+++ all-sports-app/src/App.js
@@ -14,13 +14,14 @@ function App() {
     <header className="Container">
         <h1 className='Title'>- AS -</h1>
     </header>
+     <NavBar />
+     <Homepage />
     <Routes>
         <Route path="/" element={ <Homepage /> } />
         <Route path="/sportslist" element={ <SportslistPage /> } />
         <Route path="/worldrecords" element={ <WorldRecords /> } />
         <Route path="/locations" element={ <Locations /> } />
     </Routes>
-     <NavBar />
     </>
   );
 }
