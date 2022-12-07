83aa847f7d0e14da5b08f0302f25d615bff94332
luigibustos <louisalphonsobustos@gmail.com>
2022-12-07 13:53:41 -0800

    Carousel Comp created and added to homepage

--- all-sports-app/src/App.js
+++ all-sports-app/src/App.js
@@ -8,10 +8,17 @@ import SportslistPage from "./Pages/SportslistPage";
 import WorldRecords from "./Pages/WorldRecords";
 import Locations from "./Pages/Locations";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Tags from "./Components/Tags/Tags";
-import Title from "./Components/Title";
-import { faFacebook,  faInstagram, faGithub, faTwitter, faDiscord, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome'
+// import Tags from "./Components/Tags/Tags";
+// import Title from "./Components/Title";
+// import {
+//   faFacebook,
+//   faInstagram,
+//   faGithub,
+//   faTwitter,
+//   faDiscord,
+//   faLinkedin,
+// } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "./Components/Footer";
 
 function App() {
@@ -20,7 +27,7 @@ function App() {
       <header className="Container">
         <h1 className="Title">- AS -</h1>
       </header>
-      <Title />
+      {/* <Title /> */}
       <NavBar />
       {/* <Homepage /> */}
       <Routes>
@@ -30,7 +37,7 @@ function App() {
         <Route path="/locations" element={<Locations />} />
       </Routes>
 
-        <Footer />
+      <Footer />
     </>
   );
 }
--- all-sports-app/src/Pages/SportslistPage.js
+++ all-sports-app/src/Pages/SportslistPage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Tags from '../Components/Tags/Tags'
+import React from "react";
+import { Link } from "react-router-dom";
+// import Tags from '../Components/Tags/Tags'
 
 const SportslistPage = () => {
   return (
     <>
-    <Tags />
-    <div className='sportslist'>SportslistPage</div>
+      {/* <Tags /> */}
+      <div className="sportslist">SportslistPage</div>
     </>
-  )
-}
+  );
+};
 
-export default SportslistPage
+export default SportslistPage;
