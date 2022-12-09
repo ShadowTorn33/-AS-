84bf1cafbfd86a6faa7a8675d72aaf143dea7d06
ShadowTorn33 <austincheer94@gmail.com>
2022-12-09 14:10:58 -0800

    MoreInfoPage

--- all-sports-app/src/App.js
+++ all-sports-app/src/App.js
@@ -5,10 +5,9 @@ import NavBar from "./Components/NavBar/NavBar";
 import Homepage from "./Pages/Homepage";
 import SportslistPage from "./Pages/SportslistPage";
 import WorldRecords from "./Pages/WorldRecords";
-import Locations from "./Pages/Locations";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Components/Footer";
-import Aboutme from "./Components/About Me/Aboutme";
+import MoreInfoPage from "./Components/More info/Moreinfo";
 
 function App() {
   return (
@@ -16,10 +15,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/aboutme" element={<Aboutme />} />
+        <Route path="/moreinfopage" element={<MoreInfoPage />} />
         <Route path="/sportslist/:id" element={<SportslistPage />} />
         <Route path="/worldrecords" element={<WorldRecords />} />
-        <Route path="/locations" element={<Locations />} />
       </Routes>
 
       <Footer />
