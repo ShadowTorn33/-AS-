d51c2bca202d168e55f0f81b00ff32ba75a0a841
ShadowTorn33 <austincheer94@gmail.com>
2022-12-09 15:00:28 -0800

    Table of leagues

--- all-sports-app/src/Components/NavBar/NavBar.js
+++ all-sports-app/src/Components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/moreinfopage">More Info</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/leaguespage">Leagues</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
--- all-sports-app/src/Pages/WorldRecords.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WorldRecords = () => {
-  return (
-    <div>WorldRecords</div>
-  )
-}
-
-export default WorldRecords
