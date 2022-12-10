cdc1e9161556390ba3fcac51bf0e6a80631fa3d1
ShadowTorn33 <austincheer94@gmail.com>
2022-12-09 16:31:36 -0800

    dealing with pages

--- src/Components/Leagues/Leagues.js
+++ src/Components/Leagues/Leagues.js
@@ -5,21 +5,22 @@ import Table from "react-bootstrap/Table";
 const Leagues = () => {
   const [leagues, setLeageus] = useState([]);
 
-  const fetchLeagues = () => {
-    fetch("https://www.thesportsdb.com/api/v1/json/2/all_leagues.php")
-      .then((res) => res.json())
-      .then((res) => {
-        setLeageus(res.leagues);
-      });
-  };
+//   const fetchLeagues = () => {
+//     fetch("https://www.thesportsdb.com/api/v1/json/2/all_leagues.php")
+//       .then((res) => res.json())
+//       .then((res) => {
+//         setLeageus(res.leagues);
+//       });
+//   };
 
-  useEffect(() => {
-    fetchLeagues();
-  }, []);
+//   useEffect(() => {
+//     fetchLeagues();
+//   }, []);
 
   return (
     <>
-      <Table striped bordered hover>
+    <h1>hi</h1>
+      {/* <Table striped bordered hover>
         <thead>
           <tr>
             <th></th>
@@ -40,7 +41,7 @@ const Leagues = () => {
             );
           })}
         </tbody>
-      </Table>
+      </Table> */}
     </>
   );
 };
